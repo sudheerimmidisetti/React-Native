89492bdfc87fb32e7de3eb30980b21b3ac422c8d
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2026-01-30 11:03:49 +0530

    React Native app uploaded

---
+++ app/App.js
@@ -0,0 +1,34 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello Sudheer, Your doing great!</Text>
+//       <Text>Bye Sudheer!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Aditya University</Text>
+    </View>
+  )
+}
+
+export default App;
