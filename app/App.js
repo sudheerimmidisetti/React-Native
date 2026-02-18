0b0fad795d38c568d18e63276bfd3b4759a29b64
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2026-02-18 13:13:38 +0530

    Added Chips-Task.jsx files location to run the task

--- app/App.js
+++ app/App.js
@@ -1,20 +1,29 @@
 import react from "react";
-import { View, Text, Button, TouchableOpacity, Image, StyleSheet } from "react-native";
-import MyImage from "./assets/favicon.png"
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
+import MyImage from "./assets/favicon.png";
 import { StatusBar } from "react-native";
 import Component from "./Component";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import PasswordField from "./Password-Tasks";
+import ChipInput from "./Chip-Task";
 
 const App = () => {
   return (
     <>
       <SafeAreaProvider>
         {/* <Component /> */}
-        <PasswordField />
+        {/* <PasswordField /> */}
+        <ChipInput />
       </SafeAreaProvider>
     </>
-  )
-}
+  );
+};
 
 export default App;
