a6a641e418e52a328dc1594eacc0518786410fb5
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2026-03-01 07:54:19 +0530

    Created Profile.jsx

---
+++ app/Profile.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+import { Text } from "react-native-paper";
+
+const Profile = () => {
+    return (
+        <Text>Profile-page</Text>
+    )
+}
+
+export default Profile;
