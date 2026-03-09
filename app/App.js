3e0ae040d6499e85aed22e8dd3c48fb63e594bcf
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2026-03-09 15:00:24 +0530

    Added MicroPhone.jsx path to run teh app

--- app/App.js
+++ app/App.js
@@ -18,6 +18,7 @@ import StackNavigation2 from "./Stack-Navigation-2";
 import ImagePicker from "./Image-Picker";
 import SharingImage from "./Sharing-Image";
 import CameraImageCapture from "./Camera-Image-Capture";
+import MicroPhone from "./MicroPhone";
 
 const App = () => {
   return (
@@ -30,7 +31,8 @@ const App = () => {
         {/* <StackNavigation2 /> */}
         {/* <ImagePicker /> */}
         {/* <SharingImage /> */}
-        <CameraImageCapture style={{ marginTop: 100 }}/>
+        {/* <CameraImageCapture/> */}
+        <MicroPhone />
       </SafeAreaProvider>
     </>
   );
