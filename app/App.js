7e65c56bf43a3180633fc9ef013a32296b4ae081
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2026-03-10 10:19:32 +0530

    Added path of FingerPrint-Authentication.jsx

--- app/App.js
+++ app/App.js
@@ -19,6 +19,7 @@ import ImagePicker from "./Image-Picker";
 import SharingImage from "./Sharing-Image";
 import CameraImageCapture from "./Camera-Image-Capture";
 import MicroPhone from "./MicroPhone";
+import FingerPrintAuthentication from "./FingerPrint-Authentication";
 
 const App = () => {
   return (
@@ -32,7 +33,8 @@ const App = () => {
         {/* <ImagePicker /> */}
         {/* <SharingImage /> */}
         {/* <CameraImageCapture/> */}
-        <MicroPhone />
+        {/* <MicroPhone /> */}
+        <FingerPrintAuthentication />
       </SafeAreaProvider>
     </>
   );
